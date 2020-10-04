fb5a8a044cd02f0960b9499117f74558a7063fd2
smallpythoncode <smallpythoncode@gmail.com>
2020-10-04 06:39:38 -0500

    TODO: test if docs/ is present (boolean?)

--- version_insert.py
+++ version_insert.py
@@ -1,7 +1,15 @@
 """"
-Inserts the version of projects as a comment to the top  line of
-'requirements.txt' files located in the project docs directory. This script
-should be ran from the project root (containing the README).
+A script to generate source and Sphinx requirements files with version comments
+
+1. Generates 'docs/requirements_docs.txt' to document sphinx requirements
+2. Inserts the version of the project as a comment to the top  line of
+'requirements_docs.txt' located in the project docs directory.
+
+
+Notes:
+- Script should be ran from the project root (containing the README).
+- Requires that the 'docs/' and '<source>/_version.py' be present
+  - Exceptions implemented
 """
 
 # --- Development Links ---
@@ -9,11 +17,18 @@ should be ran from the project root (containing the README).
 # https://stackoverflow.com/questions/12332975/
 # https://stackoverflow.com/questions/54462479/
 
+# Boolean
+# https://www.w3schools.com/python/python_booleans.asp
+# https://stackoverflow.com/questions/5119709/
+
+
 import os
 import subprocess
 import sys
 from _version import __version__
 
+# The packages utilized for Sphinx documentation
+# Parent installs: Sphinx, m2r2; the rest install from Sphinx
 sphinx = ['alabaster', 'Babel', 'certifi', 'chardet', 'colorama', 'docutils',
           'idna', 'imagesize', 'Jinja2', 'm2r2', 'MarkupSafe', 'mistune',
           'packaging', 'Pygments', 'pyparsing', 'pytz', 'requests', 'six',
@@ -22,27 +37,34 @@ sphinx = ['alabaster', 'Babel', 'certifi', 'chardet', 'colorama', 'docutils',
           'sphinxcontrib-jsmath', 'sphinxcontrib-qthelp',
           'sphinxcontrib-serializinghtml', 'urllib3']
 
-reqs_docs_file = "subprotest_docs.txt"
-reqs_file = "subprotest.txt"
-
-# docs_present =
-
-
-# subprocess.run([sys.executable, "-m", "pip", "freeze", ">", reqs_docs_file],
-#                shell=True)
-
 this_dir = os.getcwd()
-# print(this_dir)
-dir_split = this_dir.split("\\")
-# print(dir_split)
-# source code directory should be the same as project name
-project_name = dir_split[-1]
-print(project_name)
+reqs_docs_file = "docs/subprotest_docs.txt"
+reqs_file = "docs/subprotest.txt"
+docs_present = False
+
+
+def get_project_name():
+    dir_split = this_dir.split("\\")
+    # print(dir_split)
+    # source code directory should be the same as project name
+    project_name = dir_split[-1]
+    print(project_name)
+
+
+def docs_detect():
+    docs_present = os.path.isdir(this_dir + "\\docs")
+
+
+# def version_present():
+
+
+subprocess.run([sys.executable, "-m", "pip", "freeze", ">", reqs_docs_file],
+               shell=True)
 
 with open(reqs_docs_file, "r") as rdf:
     contents = rdf.readlines()
 
-contents.insert(0, "# " + project_name + " - v" + __version__ + "\n")
+# contents.insert(0, "# " + project_name + " - v" + __version__ + "\n")
 
 print(contents)
 
@@ -50,13 +72,9 @@ with open(reqs_docs_file, "w") as rdf:
     contents = "".join(contents)
     rdf.write(contents)
 
-list = ["a", "#", "b"]
-list.sort()
-print(list)
-
-
-
-
+# list = ["a", "#", "b"]
+# list.sort()
+# print(list)
 
 
 
@@ -65,3 +83,10 @@ print(list)
 # # path to _version.py
 #
 
+if __name__ == "__main__":
+    get_project_name()
+    docs_detect()
+    if docs_present:
+        print("Docs present")
+    else:
+        print("You fucked up")
